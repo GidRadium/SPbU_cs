9f2f89a405439e3d5635e49f4a50b009b4e1ac77
GitRadium <gidradiumcode@gmail.com>
2023-03-10 15:18:37 +0300

    Create first project

---
+++ 2023_02_17_Introduction/01_Sorting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class BubbleSort
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter numbers separated by spaces:");
+        string input = Console.ReadLine() ?? throw new Exception();
+        int[] arr = input.Split(' ').Select(int.Parse).ToArray();
+
+        bubbleSort(arr);
+        for (int i = 0; i < arr.Length; i++)
+            Console.Write(arr[i] + " ");
+        Console.WriteLine();
+    }
+
+    static void bubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
